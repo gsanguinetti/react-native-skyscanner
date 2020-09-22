e9bbccfe798bd16147cdec0e67bdb69e8151b658
Gastón Sanguinetti <gaston@sanguinetti.me>
2020-09-22 17:29:01 -0300

    Added Native base

--- src/screens/LoadingScreen/index.js
+++ src/screens/LoadingScreen/index.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import {Spinner, Container, Content, Grid} from "native-base";
-import {ActivityIndicator} from 'react-native'
 
 import { LOGIN_SCREEN } from "../../navigation/RootNavigation";
 
@@ -9,13 +8,13 @@ import styles from './style'
 export default function Loading({navigation}) {
 
     useEffect(() => {
-        //navigation.navigate(LOGIN_SCREEN)
+        navigation.navigate(LOGIN_SCREEN)
     })
 
     return (
         <Content contentContainerStyle={styles.content}>
             <Grid style={styles.grid}>
-                <ActivityIndicator size="large" color="red"/>
+                <Spinner color="red"/>
             </Grid>
         </Content>
     );
